9e7f655655bd096a5c13c4e6e36f2d9d49a1671c
LZH139 <1391546705@qq.com>
2020-04-14 20:08:49 +0800

    update

--- src/DynamicProgramming/medium/BestTimeToBuyAndSellStockWithCooldown/BestTimeToBuyAndSellStockWithCooldown.go
+++ src/DynamicProgramming/medium/BestTimeToBuyAndSellStockWithCooldown/BestTimeToBuyAndSellStockWithCooldown.go
@@ -1,5 +1,34 @@
 package BestTimeToBuyAndSellStockWithCooldown
 
 func maxProfit(prices []int) int {
+    dp := make([][]int, len(prices)+1)
 
+    days := len(prices)-1
+
+    if len(prices) <2 {
+        return 0
+    }
+
+    dp[0] = make([]int,3)
+    //0->空仓 1—>持有 2->冷冻
+    dp[0][0] = 0
+    dp[0][1] = -prices[0]
+    dp[0][2] = 0
+
+    for i:=1;i<len(prices);i++ {
+        dp[i] = make([]int,3)
+        dp[i][0] = Max(dp[i-1][0],dp[i-1][2])
+        dp[i][1] = Max(dp[i-1][0]-prices[i],dp[i-1][1])
+        dp[i][2] = dp[i-1][1]+prices[i]
+    }
+
+    return Max(dp[days][0],dp[days][2])
+
+}
+
+func Max(x int,y int) int {
+    if x>y {
+        return x
+    }
+    return y
 }
--- src/DynamicProgramming/medium/PerfectSquares/PerfectSquares.go
+++ src/DynamicProgramming/medium/PerfectSquares/PerfectSquares.go
@@ -1,5 +1,19 @@
 package PerfectSquares
 
 func numSquares(n int) int {
+    dp := make([]int,n+1)
+    for i:=1;i<=n;i++ {
+        dp[i] = i
+        for j:=1;i-j*j>=0;j++ {
+            dp[i] = Min(dp[i],dp[i-j*j]+1)
+        }
+    }
+    return dp[n]
+}
 
+func Min(x int,y int) int {
+    if x < y {
+        return x
+    }
+    return y
 }
